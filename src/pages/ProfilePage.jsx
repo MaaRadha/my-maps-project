9b39df03976bef02075b3d73b886f8236ec0dcae
MaaRadha <Omyoga2016@outlook.com>
2025-02-07 13:41:38 +0100

    added correct components

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,12 +1,18 @@
-import React from "react";
-import Dashboard from "../components/profile/Dashboard";
-import MyComponent from "../components/map/Mycomponent";
+import Maps from "../components/profile/Maps";
+import Globe from "../components/map/Globe";
 
 const ProfilePage = () => {
   return (
-    <div>
-      {/* <Dashboard /> */}
-      <MyComponent />
+    <div className="container mx-auto max-w-7xl p-4 min-h-screen">
+      {/* Use grid with 2 columns on medium (md) and larger screens */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 h-full">
+        <div className="h-screen">
+          <Maps />
+        </div>
+        <div className="h-screen">
+          <Globe />
+        </div>
+      </div>
     </div>
   );
 };
