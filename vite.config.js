b0a858501ec4f5aa245c6839f49ffc66cd7a3ae5
MaaRadha <Omyoga2016@outlook.com>
2025-02-07 02:29:47 +0100

    redirect file updated

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: "localhost",
-    port: 5173, // Change if needed
-    strictPort: true,
-    hmr: {
-      clientPort: 5173, // Ensure WebSocket runs properly
-    },
-  },
+  // server: {
+  //   host: "localhost",
+  //   port: 5173, // Change if needed
+  //   strictPort: true,
+  //   hmr: {
+  //     clientPort: 5173, // Ensure WebSocket runs properly
+  //   },
+  // },
   plugins: [tailwindcss(), react()],
 });
